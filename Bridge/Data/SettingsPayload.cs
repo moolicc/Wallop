54c8cec0efdc897f4e7735c957c7e791b9492d4d
Joel <moolick@outlook.com>
2019-12-25 18:09:55 -0600

    SettingsPayload created

---
+++ Bridge/Data/SettingsPayload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WallApp.Bridge.Data
+{
+    public class SettingsPayload : IPayload
+    {
+        public int LayerId { get; set; }
+        public bool Enabled { get; set; }
+        public float Rotation { get; set; }
+        public string Effect { get; set; }
+        public (byte, byte, byte) Tint { get; set; }
+
+        public SettingsPayload(int layerId, bool enabled, float rotation, string effect, (byte, byte, byte) tint)
+        {
+            LayerId = layerId;
+            Enabled = enabled;
+            Rotation = rotation;
+            Effect = effect;
+            Tint = tint;
+        }
+    }
+}
