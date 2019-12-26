2c917153bb710aa19adbafc3c039c571e90fa1eb
Joel <moolick@outlook.com>
2019-12-25 18:17:00 -0600

    Added opacity

--- Bridge/Data/SettingsPayload.cs
+++ Bridge/Data/SettingsPayload.cs
@@ -11,14 +11,16 @@ namespace WallApp.Bridge.Data
         public float Rotation { get; set; }
         public string Effect { get; set; }
         public (byte R, byte G, byte B) Tint { get; set; }
+        public float Opacity { get; set; }
 
-        public SettingsPayload(int layerId, bool enabled, float rotation, string effect, (byte, byte, byte) tint)
+        public SettingsPayload(int layerId, bool enabled, float rotation, string effect, (byte, byte, byte) tint, float opacity)
         {
             LayerId = layerId;
             Enabled = enabled;
             Rotation = rotation;
             Effect = effect;
             Tint = tint;
+            Opacity = opacity;
         }
     }
 }
