097e6416625d8d259c523255996f6190c4c66c49
Joel <moolick@outlook.com>
2020-01-28 15:43:02 -0600

    Added in some Type loader stuff mostly

---
+++ Types/Types/Loading/TypeLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+namespace Wallop.Types.Loading
+{
+    public class TypeLoader
+    {
+        public T LoadFromLibrary<T>(string library)
+        {
+            var assembly = Assembly.LoadFile(library);
+            var types = assembly.GetTypes();
+            Type targetType = typeof(T);
+            Type implementorType = null;
+
+            foreach (var item in types)
+            {
+                if(item.IsAssignableFrom(targetType))
+                {
+                    implementorType = item;
+                    break;
+                }
+            }
+
+            if(implementorType == null)
+            {
+                return default(T);
+            }
+
+            return (T)Activator.CreateInstance(implementorType);
+        }
+    }
+}
---
+++ Types/UIExtensions/Field.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wallop.UIExtensions
+{
+    public class Field
+    {
+        public string Label { get; set; }
+
+        public int FieldType { get; set; }
+
+        public object Argument { get; set; }
+
+        public object DefaultValue { get; set; }
+
+    }
+}
