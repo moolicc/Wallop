642d4a68b7b25989ec60582c1ded55abc8bbfc68
Joel <moolick@outlook.com>
2020-02-02 05:45:58 -0600

    Added IPCClient abstract type

---
+++ Types/IPC/IPCClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wallop.IPC
+{
+    public abstract class IPCClient
+    {
+        public abstract string[] GetOptionNames();
+
+        public abstract bool CreateActiveConnection(string connectionName, (string, string) options);
+
+        public abstract void DestroyConnection(string connectionName);
+
+        public abstract bool SendData(string connectionName, object data);
+    }
+}
