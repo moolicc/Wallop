a756423f87c15da16b06cf768e2914f41a1128c5
Joel <moolick@outlook.com>
2022-07-21 08:43:22 -0500

    Project created

---
+++ src/PackageGen/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.CommandLine;
+
+Console.WriteLine("Hello, World!");
+
+var command = BuildCommandTree();
+
+bool repl = true;
+while (repl)
+{
+    var input = Console.ReadLine();
+
+
+    repl = false;
+}
+
+
+RootCommand BuildCommandTree()
+{
+    var root = new RootCommand("");
+
+    return root;
+}
