45b5467620ca3e5e727439c4f82705ccaa42bd97
Joel <moolick@outlook.com>
2022-07-27 20:53:59 -0500

    Fixed plugin

--- src/Plugins/EnginePlugins/JsonCommandPlugin.cs
+++ src/Plugins/EnginePlugins/JsonCommandPlugin.cs
@@ -2,6 +2,7 @@ using EnginePlugins.Overlay.Windows;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -10,29 +11,38 @@ using Wallop.Types.Plugins.EndPoints;
 
 namespace EnginePlugins
 {
+
     public class JsonCommandPlugin
     {
         [PluginPantry.Extending.PluginEntryPoint("Json Command", "1.0.0.0")]
         public void PluginEntryPoint(PluginPantry.Extending.PluginInformation pluginInfo)
         {
-            pluginInfo.Exposed.RegisterEndPoint<Wallop.Types.Plugins.EndPoints.EngineStartupEndPoint>(HandleCommandLine, pluginInfo.PluginId);
+            pluginInfo.Exposed.RegisterEndPoint<EngineStartupEndPoint>(HandleCommandLine, pluginInfo.PluginId);
         }
 
         public void HandleCommandLine(Wallop.Types.Plugins.EndPoints.EngineStartupEndPoint context)
         {
-            var sourceArg = new Argument<string>("source", "The source or filepath to the source of the json.");
+            var sourceArg = new Argument<string>("source", description: "The source or filepath to the source of the json.");
             var command = new Command("json", "Executes commands contained in a json string or file.")
             {
                 sourceArg
             };
+            //command.Handler = new MyHandler(context, sourceArg);
+
+
             command.SetHandler(s =>
             {
-                if(File.Exists(s))
+
+                if (File.Exists(s))
                 {
                     s = File.ReadAllText(s);
                 }
+                else
+                {
+                    // NOTE: This allows strings provided via raw json within the command line argument to escape quotes around json values with single quotes.
+                    s = s.Replace('\'', '"');
+                }
 
-                
                 var messages = Wallop.Messaging.Messages.Json.Json.ParseMessages(s);
 
                 foreach (var item in messages)
