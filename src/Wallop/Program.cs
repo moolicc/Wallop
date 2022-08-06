8485c7d085c08f837d8990564408cf6bf3c9aeec
Joel <moolick@outlook.com>
2022-08-05 21:48:07 -0500

    Multiple instances!

--- src/Wallop/Program.cs
+++ src/Wallop/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 using System.Reflection;
 using Wallop;
 using Wallop.ECS;
@@ -8,8 +10,15 @@ namespace Wallop
 {
     class Program
     {
+        public const string APP_RESOURCE_DELIMITER = "-";
+        public const string DEFAULT_NAME = "wallop.exe";
+        public const string MY_NAME_DIRECTIVE = "my-name";
+        public const string SEND_TO_DIRECTIVE = "send-to";
+
         private const string DEFAULT_CONF = "engine.json";
-        private const string MUTEX_NAME = "WallopSingleInstanceMutex";
+
+        private const string ARGS_PIPE_RESOURCE = "args";
+        private const string MUTEX_RESOURCE = "single-instance-mutex";
 
         private static EngineApp? _app;
         private static string _confFile = DEFAULT_CONF;
@@ -19,27 +28,51 @@ namespace Wallop
 
         static int Main(string[] args)
         {
-            using (var mutex = new Mutex(true, MUTEX_NAME, out var isOnlyInstance))
+            string targetApp = DEFAULT_NAME;
+            if(args.Length >= 1)
+            {
+                if (args[0].StartsWith($"[{SEND_TO_DIRECTIVE}"))
+                {
+                    if (args.Length == 1)
+                    {
+                        return 0;
+                    }
+                    targetApp = args[0].TrimEnd(']').Substring(args[0].IndexOf(':') + 1);
+                    args = args[1..];
+                }
+                else if (args[0].StartsWith($"[{MY_NAME_DIRECTIVE}"))
+                {
+                    targetApp = args[0].TrimEnd(']').Substring(args[0].IndexOf(':') + 1);
+                    args = args[1..];
+
+                    Settings.AppSettings.InstanceName = targetApp;
+                }
+            }
+
+            using (var mutex = new Mutex(true, $"{targetApp}{APP_RESOURCE_DELIMITER}{MUTEX_RESOURCE}", out var isOnlyInstance))
             {
                 try
                 {
                     if(isOnlyInstance)
                     {
-                        var pipe = new IPC.PipeHost("wallop.exe", "FIRSTINSTANCE");
+                        var pipe = new IPC.PipeHost($"{Settings.AppSettings.InstanceName}", $"{Settings.AppSettings.InstanceName}{APP_RESOURCE_DELIMITER}{ARGS_PIPE_RESOURCE}");
                         pipe.Begin();
 
                         var host = new IPC.IpcNode(pipe);
 
                         host.OnDataReceived2 = OtherInstanceMessageReceived;
                     }
-                    else if(args.Length > 0)
+                    else if(!isOnlyInstance && args.Length > 0)
                     {
                         EngineLog.For<Program>().Warn("Another instance of the application has been detected. Forwarding command line and exiting.");
 
-                        var pipe = new IPC.PipeClient($"wallop.exe-{DateTime.Now.Ticks}", "FIRSTINSTANCE");
+                        var targetResource = ARGS_PIPE_RESOURCE;
+
+
+                        var pipe = new IPC.PipeClient($"{DEFAULT_NAME}-{DateTime.Now.Ticks}", $"{targetApp}{APP_RESOURCE_DELIMITER}{targetResource}");
                         var client = new IPC.IpcNode(pipe);
 
-                        client.Send(args.Aggregate((s1, s2) => s1 + ' ' + s2), "wallop.exe");
+                        client.Send(args.Aggregate((s1, s2) => s1 + ' ' + s2), targetApp);
                         if(client.GetReply(TimeSpan.MaxValue, out var reply))
                         {
                             Console.WriteLine(reply);
@@ -48,11 +81,11 @@ namespace Wallop
 
                         return 0;
                     }
-                    else
+                    else if(!isOnlyInstance && args.Length <= 0)
                     {
                         return 0;
                     }
-                    RunProgram();
+                    RunProgram(isOnlyInstance);
                 }
                 catch (Exception ex)
                 {
@@ -82,7 +115,7 @@ namespace Wallop
             node.Send(results ?? "", message.SourceApplication);
         }
 
-        private static void RunProgram()
+        private static void RunProgram(bool isFirstInstance)
         {
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(LoadDep);
 
@@ -91,7 +124,7 @@ namespace Wallop
             {
                 commandLine = string.Join(" ", Environment.GetCommandLineArgs()[1..]);
             }
-            ExecuteCommandLine(true, commandLine, null);
+            ExecuteCommandLine(isFirstInstance, commandLine, null);
 
 
             var context = new PluginPantry.PluginContext();
@@ -107,7 +140,8 @@ namespace Wallop
             {
                 EngineLog.For<Program>().Info("Setting up Engine Application...");
 
-                ExecuteCommandLine(true, commandLine, null);
+                isFirstInstance = !isFirstInstance;
+                ExecuteCommandLine(isFirstInstance, commandLine, null);
 
                 EngineLog.For<Program>().Info("Running Engine...");
                 _app.Run();
@@ -194,7 +228,34 @@ namespace Wallop
                 root = new RootCommand();
             }
             BuildProgramCommandTree(root);
-            root.Invoke(commandLine.Trim(), console);
+
+            var builder = new CommandLineBuilder(root);
+            builder.AddMiddleware(async (context, next) =>
+            {
+                if (context.ParseResult.Directives.TryGetValues(MY_NAME_DIRECTIVE, out var values))
+                {
+                    if (values.Count != 1)
+                    {
+                        //TODO: Error
+                    }
+                    //Settings.AppSettings.InstanceName = values[0];
+                }
+                await next(context);
+            });
+            builder.UseDefaults();
+            builder.Build().Invoke(commandLine.Trim(), console);
+
+            //root.Invoke(commandLine.Trim(), console);
+
+            if(firstInstance)
+            {
+                //var pipe = new IPC.PipeHost($"{Settings.AppSettings.InstanceName}", $"{Settings.AppSettings.InstanceName}{APP_RESOURCE_DELIMITER}{ARGS_PIPE_RESOURCE}");
+                //pipe.Begin();
+
+                //var host = new IPC.IpcNode(pipe);
+
+                //host.OnDataReceived2 = OtherInstanceMessageReceived;
+            }
         }
 
         private static void BuildProgramCommandTree(RootCommand root)
